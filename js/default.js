cfaa68a214cfb88b73c011763c11dc5d0f6f566a
Sachico <g1627059@st.t-kougei.ac.jp>
2017-06-26 17:48:07 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,6 @@
-
-
+for(var i=1; i< 100; i++){
 var li =document.createElement('li');
-li.textContent ='アイテム';
+li.textContent =i + '回目';
 document.getElementById('list').appendChild(li);
 document.getElementById('form').onsubmit = function(){
  window.alert(document.getElementById('form').word.value);
