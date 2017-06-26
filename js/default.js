71ae948029144b9148e9ffaf14a85d1ec1259147
Sachico <g1627059@st.t-kougei.ac.jp>
2017-06-26 17:59:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -7,13 +7,7 @@ for(var i=1; i< 100; i++){array[i] = i;
         array.splice(i, 1 , ["Fizz"]);
  
 var li =document.createElement('li');
-li.textContent =i + 'for(var i=1; i< 100; i++){array[i] = i; 
-      if(array[i] % 15 === 0) 
-        array.splice(i, 1 , ["FizzBuzz"]); 
-      else if (array[i] % 5 === 0) 
-        array.splice(i, 1 , ["Buzz"]); 
-      else if (array[i] % 3 === 0) 
-        array.splice(i, 1 , ["Fizz"]);';
+li.textContent =i + '回目';
 document.getElementById('list').appendChild(li);
 document.getElementById('form').onsubmit = function(){
  window.alert(document.getElementById('form').word.value);
